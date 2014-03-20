41b7eb1d7fffa28eead77e332fe943abcf61f160
James Fitzsimmons <jrfitzsimmons@gmail.com>
2014-03-21 10:17:03 +1100

    Started work on pretty printer

--- pretty.c
+++ pretty.c
@@ -10,9 +10,41 @@
 
 extern void report_error_and_exit(const char *msg);
 
+void print_program(FILE *, Program *);
+void print_declarations(FILE *, Decls *);
+void print_statements(FILE *, Stmts *);
+
 void
 pretty_prog(FILE *fp, Program *prog) {
 
-    report_error_and_exit("Pretty-print has not been implemented yet");
+    print_program(fp, prog);
+    //report_error_and_exit("Pretty-print has not been implemented yet");
 }
 
+void print_program(FILE *fp, Program *program) {
+
+    print_declarations(fp, program->decls);
+    fprintf(fp, "\n");
+    print_statements(fp, program->body);
+}
+
+void print_declarations(FILE *fp, Decls *declarations) {
+
+    if (declarations == NULL) {
+        return;
+    }
+
+    Decl *decl = declarations->first;
+    fprintf(fp, "%s\n", decl->id);
+
+    print_declarations(fp, declarations->rest);
+}
+
+void print_statements(FILE *fp, Stmts *statements) {
+
+    if (statements == NULL) {
+        return;
+    }
+
+
+}
