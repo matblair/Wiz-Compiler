2e332c39f5aa49338657843b951432684780bf56
Aparna Elangovan <aparnaelangovan@aparnas-air.bigpond>
2014-03-26 23:04:29 +1100

    Moved code from wiz to wizparser to run tests

---
+++ src/wizparser.c
@@ -0,0 +1,96 @@
+/* wiz.c */
+
+/*-----------------------------------------------------------------------
+    A driver for a pretty-printer for Iz, a subset of Wiz.
+    For use the COMP90045 project 2014.
+-----------------------------------------------------------------------*/
+
+#include    <string.h>
+#include    <stdlib.h>
+#include    "ast.h"
+#include    "std.h"
+#include    "pretty.h"
+#include    "missing.h"
+
+const char  *progname;
+const char  *iz_infile;
+Program     *parsed_program = NULL;
+
+extern FILE *yyin;
+
+static void usage(void);
+void        report_error_and_exit(const char *msg);
+void        *checked_malloc(int num_bytes);
+
+int
+wizparser(int argc, char **argv) {
+
+    const char  *in_filename;
+    FILE        *fp = stdout;
+    BOOL        pretty_print_only;
+
+    progname = argv[0];
+    pretty_print_only = FALSE;
+
+    /* Process command line */
+
+    if ((argc < 2) || (argc > 3)) {
+        usage();
+        exit(EXIT_FAILURE);
+    }
+
+    if (argc == 2)
+        in_filename = argv[1];
+
+    if (argc == 3 && streq(argv[1],"-p")) {
+        pretty_print_only = TRUE;
+        in_filename = argv[2];
+    }
+    
+    iz_infile = in_filename;
+    yyin = fopen(in_filename, "r");
+    if (yyin == NULL) {
+        perror(in_filename);
+        exit(EXIT_FAILURE);
+    }
+
+    if (yyparse() != 0) {
+        /* The error message will already have been printed. */
+        exit(EXIT_FAILURE);
+    }
+
+    if (pretty_print_only) 
+        pretty_prog(fp, parsed_program);
+    else
+        report_error_and_exit("Unable to generate code");
+
+    return 0;
+}
+
+/*---------------------------------------------------------------------*/
+
+static void
+usage(void) {
+
+    printf("usage: wiz [-p] iz_source_file\n");
+}
+
+void
+report_error_and_exit(const char *msg) {
+
+    fprintf(stderr, "Error: %s\n", msg);
+    exit(EXIT_FAILURE);
+}
+
+void *
+checked_malloc(int num_bytes) {
+
+    void *addr;
+
+    addr = malloc((size_t) num_bytes);
+    if (addr == NULL) 
+        report_error_and_exit("Out of memory");
+    return addr;
+}
+
+/*---------------------------------------------------------------------*/
--- src/wizparser.h
+++ src/wizparser.h
@@ -9,6 +9,7 @@ extern  Program *parsed_program;
 
 static  void    report_error_and_exit(const char *msg);
 extern  void    *checked_malloc(int num_bytes);
+int wizparser(int argc, char **argv) ;
 
 #endif /* WIZ */
 
